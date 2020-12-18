cadf6fcc3428c81d2e97c97f5b515ed5b742a007
Nataliapiesn <01123786@pw.edu.pl>
2020-12-18 19:12:47 +0000

    Task 1,2 completed.

--- lab7.cpp
+++ lab7.cpp
@@ -1,6 +1,22 @@
+#include <exception>
 #include <iostream>
+#include <stdexcept>
+#include <vector>
+
+using namespace std;
 
 int main()
 {
-    puts("Hello, World!");
+    long int n;
+    cin >> n;
+    try {
+        vector< long int > numbers;
+        numbers.assign(n, 0);
+    }
+    catch (const std::bad_alloc& e) {
+        cout << e.what() << endl;
+    }
+    catch (...) {
+        cout << "Default exception." << endl;
+    }
 }
