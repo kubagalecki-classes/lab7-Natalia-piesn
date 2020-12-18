c57285a69df7435d78869ba075d87b2d41b05ed1
Nataliapiesn <01123786@pw.edu.pl>
2020-12-18 19:31:09 +0000

    Task 3 completed.

--- lab7.cpp
+++ lab7.cpp
@@ -1,20 +1,31 @@
-#include <exception>
 #include <iostream>
 #include <stdexcept>
+#include <variant>
 #include <vector>
 
 using namespace std;
 
 int main()
 {
-    long int n;
-    cin >> n;
+    long int a, b;
+    cin >> a >> b;
     try {
         vector< long int > numbers;
-        numbers.assign(n, 0);
+        numbers.assign(a, 0);
+        variant< int, std::string > v;
+        if (b % 2 == 0) {
+            v = 42;
+        }
+        else {
+            v = "odd";
+        }
+        cout << get< int >(v) << endl;
     }
-    catch (const std::bad_alloc& e) {
-        cout << e.what() << endl;
+    catch (const bad_alloc& e) {
+        cout << e.what() << " alloc" << endl;
+    }
+    catch (const bad_variant_access& e) {
+        cout << e.what() << " var" << endl;
     }
     catch (...) {
         cout << "Default exception." << endl;
